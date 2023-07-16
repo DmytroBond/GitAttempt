c92e2bd727b725a6cc0fe92f1f575f06a184d40e
DmytroBond <bond_dv@ukr.net>
2023-07-16 18:27:26 +0300

    second commit

--- GitAttempt.java
+++ GitAttempt.java
@@ -5,6 +5,7 @@ public class GitAttempt {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("It's GitAttempt...");
+		System.out.println("It's GitAttempt...");
 	}
 
 }
